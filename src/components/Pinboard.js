0ec0ce3070917edb441b222a6ec5d30b80b8dff1
Kristian Hamilton <krisham87@hotmail.com>
2018-06-22 21:08:08 +0200

    introduce uuid and remove posit functionality

--- src/components/Pinboard.js
+++ src/components/Pinboard.js
@@ -1,13 +1,34 @@
 import React from "react";
+import uuidv1 from "uuid/v1";
 
 const Pinboard = props => {
+  const newNote = function(noteId) {
+    return {
+      noteText: "",
+      noteColour: "yellow",
+      id: noteId,
+      x: 100,
+      y: 100
+    };
+  };
+
   return (
     <div>
       <h1>Pinboard</h1>
+      <button onClick={() => props.createNote(newNote(uuidv1()))}>
+        Create new note
+      </button>
       <ul>
         {props.notes &&
           props.notes.map(note => {
-            return <li key={note.id}>{note.noteText}</li>;
+            return (
+              <li key={note.id}>
+                {note.noteText}
+                <button onClick={() => props.removeNote(note.id)}>
+                  Remove note
+                </button>
+              </li>
+            );
           })}
       </ul>
     </div>
--- src/components/pinboardContainer.js
+++ src/components/pinboardContainer.js
@@ -1,5 +1,5 @@
 import { connect } from "react-redux";
-import { createNote } from "../actions/actions";
+import { createNote, removeNote } from "../actions/actions";
 
 import Pinboard from "./Pinboard";
 
@@ -13,7 +13,8 @@ const PinboardContainer = connect(
   },
   function mapDispatchToProps(dispatch) {
     return {
-      createNote: note => dispatch(createNote(note))
+      createNote: note => dispatch(createNote(note)),
+      removeNote: id => dispatch(removeNote(id))
     };
   }
 )(Pinboard);
